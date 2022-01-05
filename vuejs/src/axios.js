8b0ccba75af270d444d2f1039fb1f426620daf4b
Mohamed Ibrahem <mohamed.ibr1994@gmail.com>
2022-01-05 15:22:35 +0200

    update backend baseURL in axios

--- vuejs/src/axios.js
+++ vuejs/src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem('app_user_token');
 
 export default axios.create({
-	baseURL: 'https://sdc.test/integrations/v2/10',
+	baseURL: 'https://test.sdcappsa.com/integrations/v2/1',
 	headers: {
 		Authorization: `Bearer ${token}`
 	}
